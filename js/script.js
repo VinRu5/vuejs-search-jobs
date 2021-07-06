5b39cd5962c9b181acf3854fdfb403b5f66c4276
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-07-06 11:15:56 +0200

    Inizio

---
+++ js/script.js
@@ -0,0 +1,75 @@
+Vue.config.devtools = true;
+
+new Vue(
+    {
+        el: '#app',
+        data: {
+            jobs: [
+                {
+                    id: 1,
+                    company: 'Perferendis',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 2,
+                    company: 'NTT Data',
+                    position: 'Web Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/28/2021',
+                    logo: 'logo.jpg',
+                    city: 'Cosenza',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 3,
+                    company: 'Fiat',
+                    position: 'Operaio',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '6/04/2021',
+                    logo: 'logo.jpg',
+                    city: 'Milano',
+                    contract: 'Part Time'
+                },
+                {
+                    id: 4,
+                    company: 'Vobis',
+                    position: 'Sistemista',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/16/2021',
+                    logo: 'logo.jpg',
+                    city: 'Rimini',
+                    contract: 'Part Time'
+                },
+                {
+                    id: 5,
+                    company: 'WebHeart',
+                    position: 'Full-stack Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/20/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 6,
+                    company: 'Sirio',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                }
+            ],
+            starred: [1, 2, 3],
+            applied: [4, 5]
+
+        }
+
+    }
+)
